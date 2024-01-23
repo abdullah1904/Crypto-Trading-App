cce2b1254019271414749cec053f9effdee6842b
Muhammad Abdulllah <mabdullahzahid782@gmail.com>
2024-01-23 11:29:39 +0500

    Created Main Menu

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char option;
+    cout<<"   --: Menu :--"<<endl<<endl;
+    cout<<"1) Print help"<<endl;
+    cout<<"2) Print exchange stats"<<endl;
+    cout<<"3) Place an ask"<<endl;
+    cout<<"4) Place a bid"<<endl;
+    cout<<"5) Print Wallet"<<endl;
+    cout<<"6) continue"<<endl<<endl;
+    cout<<"Select option here: ";
+    cin>>option;
+    cout<<"Chose: "<<option<<endl;
+    return 0;
+}
