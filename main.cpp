aa824c1f2268823d1bef3fd78fa9202c20d83ee5
Muhammad Abdulllah <mabdullahzahid782@gmail.com>
2024-01-25 01:32:16 +0500

    Loop added

--- main.cpp
+++ main.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
 using namespace std;
 int main(){
-    char option;
-    cout<<"   --: Menu :--"<<endl<<endl;
-    cout<<"1) Print help"<<endl;
-    cout<<"2) Print exchange stats"<<endl;
-    cout<<"3) Place an ask"<<endl;
-    cout<<"4) Place a bid"<<endl;
-    cout<<"5) Print Wallet"<<endl;
-    cout<<"6) continue"<<endl<<endl;
-    cout<<"Select option here: ";
-    cin>>option;
-    cout<<"Chose: "<<option<<endl;
+    while (true){
+        int option;
+        cout << "   --: Menu :--" << endl<< endl;
+        cout << "1) Print help" << endl;
+        cout << "2) Print exchange stats" << endl;
+        cout << "3) Place an ask" << endl;
+        cout << "4) Place a bid" << endl;
+        cout << "5) Print Wallet" << endl;
+        cout << "6) Continue" << endl<< endl;
+        cout << "Select option here: ";
+        cin >> option;
+        cout << "Chose: " << option << endl;
+        if (option == 0){
+            cout << "Invalid Choice! Enter Valid Option" << endl;
+        }
+        else if (option == 1){
+            cout << "Help - Your aim is to make money. Analyze the market and make bids and offers" << endl;
+        }
+        else if (option == 2){
+            cout << "Market looks good" << endl;
+        }
+        else if (option == 3){
+            cout << "Mark and offer - enter the amount" << endl;
+        }
+        else if (option == 4){
+            cout << "Make a bid - enter the amount" << endl;
+        }
+        else if (option == 5){
+            cout << "Your wallet is empty" << endl;
+        }
+        else if (option == 6){
+            cout << "Going to next time frame" << endl;
+        }
+        cout<<endl;
+    }
     return 0;
 }
